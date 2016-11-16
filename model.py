996ff4af64351ab014f1ce8924e1aa2c67f3793b
Paul Galloway <galloway.paul.f@gmail.com>
2016-11-15 19:24:58 -0800

    Altered network to be more like nvidia paper but with max pooling...amazingly still not producing results

--- model.py
+++ model.py
@@ -5,6 +5,8 @@ import time
 from scipy import ndimage
 import numpy as np
 import tensorflow as tf
+flags = tf.app.flags
+flags.FLAGS.CUDA_VISIBLE_DEVICES = ''
 from resnet import ResNetBuilder
 
 from keras import backend as K
@@ -38,43 +40,53 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import KFold, cross_val_score
 
 
-DRIVING_TYPES = ['corrective', 'inline']#, 'mixed'] # Choose what type of data to sample
-COURSES = ['flat', 'inclines'] # Randomly sample from this
+DRIVING_TYPES = ['mixed']#, 'corrective', 'inline'] # Choose what type of data to sample
+COURSES = ['flat'] #, 'inclines'] # Randomly sample from this
 # These two lists represent a tree: top level choose flat or inclines
 # Second level choose mixed, corrective or inline
 
 BASE_PATH = '/home/paul/workspace/keras-resnet-sdc/recorded_data'
+MINI_BATCH_SIZE = 64
 
-def base_model(): 
-    #input = Input(shape=(3, 320, 160))
+# needed, because mse and mae just produce a network that predicts the mean angle
+def sum_squared_error(y_true, y_pred):
+    return K.sum(K.square(y_pred - y_true), axis=0)
+
+def base_model():
+    overall_activation = 'relu' 
 
     model = Sequential()
-    # this applies 32 convolution filters of size 3x3 each.
-    model.add(Convolution2D(16, 3, 3, border_mode='valid', input_shape=(160, 320, 3),
-                            dim_ordering='tf'))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(16, 3, 3))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(4,4)))
-    model.add(Dropout(0.25))
-
-    model.add(Convolution2D(16, 3, 3, border_mode='valid'))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(16, 3, 3))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2))) 
-    model.add(Dropout(0.25))
-
+    # From Nvidia paper
+    #model.add(BatchNormalization(input_shape=(160, 320, 3),
+    #                             epsilon=1e-06, mode=0, momentum=0.9, weights=None))
+    model.add(Convolution2D(24, 5, 5, input_shape=(160, 320, 3), 
+                            border_mode='valid', dim_ordering='tf'))
+    model.add(MaxPooling2D(strides=(4,4)))
+    model.add(Activation(overall_activation))
+    model.add(Convolution2D(36, 5, 5))
+    model.add(Activation(overall_activation))
+    model.add(Convolution2D(48, 5, 5))
+    model.add(Activation(overall_activation))
+    model.add(Dropout(0.2))
+    model.add(Convolution2D(64, 3, 3))
+    model.add(Activation(overall_activation))
+    model.add(Convolution2D(64, 3, 3))
+    model.add(Activation(overall_activation))
+    model.add(MaxPooling2D(strides=(4,4)))
     model.add(Flatten())
+    
+    model.add(Dense(1164))
+    model.add(Activation(overall_activation))
+    model.add(Dropout(0.1))
     model.add(Dense(100))
-    model.add(Activation('relu'))
-    #model.add(Dropout(0.5))
-    model.add(Dense(100))
-    model.add(Activation('relu'))
+    model.add(Activation(overall_activation))
+    model.add(Dropout(0.1))
+    model.add(Dense(50))
+    model.add(Activation(overall_activation))
+    model.add(Dense(10))
 
-    #model.add(Lambda(
-    model.add(Dense(1))#init='normal'))
-    model.add(Activation('linear')) 
+    model.add(Dense(1))
+    model.add(Activation(overall_activation)) 
 
     return model
 
@@ -85,12 +97,20 @@ def load_batch(course_data, course_name, batches_so_far, batch_size, test=False)
     driving_type = np.random.choice(DRIVING_TYPES)
     driving_type_data = course_data[driving_type]
 
+    exhausted = False
     if not test: 
-        s_index = batches_so_far*batch_size
-        if s_index + batch_size*2 > len(driving_type_data):
-            s_index = s_index % len(driving_type_data)
+        #coin = 0 #np.random.randint(0,2)
+        # Choose which side of the data to grab (0=beginning, 1=ending)
+        # Older vs. newer
+        if True: #if coin == 0:
+            s_index = batches_so_far*batch_size
+            if s_index + batch_size*2 > len(driving_type_data):
+                s_index = s_index % len(driving_type_data)
+                exhausted = True
 
-        sub_list = driving_type_data[s_index:s_index+batch_size]
+            sub_list = driving_type_data[s_index:s_index+batch_size]
+        #else:
+        #    sub_list = driving_type_data[-1:-1-batch_size*2]
         data, labels = build_batch(batch_size, sub_list, course_name, driving_type)
     else:
         batch_size = int(0.3*batch_size)
@@ -100,7 +120,7 @@ def load_batch(course_data, course_name, batches_so_far, batch_size, test=False)
     print("Fetching batch of size %s for the %s course with driving type %s" % (batch_size, 
                                                                       course_name, driving_type))
 
-    return data, labels
+    return exhausted, data, labels
 
 def build_batch(batch_size, sub_list, course_name, driving_type):
 
@@ -112,7 +132,7 @@ def build_batch(batch_size, sub_list, course_name, driving_type):
         row = sub_list[current_size]
         if "," not in row: 
             print ("Invalid csv row, no comma(s)")
-            continue
+            break # Only thing that works
         values = row.split(",")
         if len(values) != 7: 
             print ("Invalid csv line, values != 7")
@@ -135,20 +155,20 @@ def build_batch(batch_size, sub_list, course_name, driving_type):
     return data, labels            
 
 
-def load_data(csv_lists, batches_so_far=0, batch_size=1024, test=False):
+def load_data(csv_lists, batches_so_far=0, batch_size=512, test=False):
 
     # Randomly choose between the courses
     course_name = np.random.choice(COURSES)
     
     course_data = csv_lists[course_name]
    
-    X_train, y_train = load_batch(course_data, course_name, batches_so_far, 
+    exhausted, X_train, y_train = load_batch(course_data, course_name, batches_so_far, 
                                   batch_size=batch_size)
 
     y_train = np.reshape(y_train, (len(y_train), 1))
  
     if test:
-        X_test, y_test = load_batch(course_data, course_name, batches_so_far, 
+        _, X_test, y_test = load_batch(course_data, course_name, batches_so_far, 
                                    batch_size=batch_size, test=True)
         y_test = np.reshape(y_test, (len(y_test), 1))
     else:
@@ -156,20 +176,20 @@ def load_data(csv_lists, batches_so_far=0, batch_size=1024, test=False):
         y_test = None
 
 
-    return (X_train, y_train), (X_test, y_test)
+    return exhausted, (X_train, y_train), (X_test, y_test)
 
 
 def main():
 
-    mini_batch_size = 64
+    mini_batch_size = MINI_BATCH_SIZE
     nb_epoch = 20
     data_augment = False
 
     model = base_model()
     #model = ResNetBuilder.build_resnet_18((3, 320, 160), 1)
-    model.compile(lr=0.01, 
-                  loss='mse', 
-                  optimizer='RMSprop', #'adam'
+    model.compile(#lr=0.01, 
+                  loss=sum_squared_error, #'mse', 
+                  optimizer='adam', #'RMSprop', #'adam'
                   ) # RMSprop'),
     model.summary()
 
@@ -226,13 +246,14 @@ def main():
     print("fitting the model on the batches generated by datagen.flow()")
     exhausted = False # Means we have gone through all of the training set
     batches = 0
-    (X_train, y_train), (X_test, y_test) = load_data(csv_lists, 
+    exhausted, (X_train, y_train), (X_test, y_test) = load_data(csv_lists, 
                                                      batches_so_far=batches,
                                                      test=True)
-    #X_test = X_test.astype('float32')
-    #X_test = (X_test - np.mean(X_test))/np.std(X_test)
-     
-    while batches < 100:
+    X_test = X_test.astype('float32')
+    X_test = (X_test - np.mean(X_test))/np.std(X_test)
+    epoch = 1
+    print("Starting first epoch")
+    while epoch <= 10:
         batches += 1
         print('Starting batch %s' % batches)
         X_train = X_train.astype('float32')
@@ -240,13 +261,16 @@ def main():
 
         model.fit_generator(datagen.flow(X_train, y_train, 
                                          batch_size=mini_batch_size),
-                            samples_per_epoch=len(X_train), nb_epoch=10, 
+                            samples_per_epoch=len(X_train), nb_epoch=1, # Epochs on subsample 
                             validation_data=(X_test, y_test), 
                             callbacks=[checkpointer])
                             
-        (X_train, y_train), _ = load_data(csv_lists, 
+        exhausted, (X_train, y_train), _ = load_data(csv_lists, 
                                           batches_so_far=batches)
         
+        if exhausted:
+            epoch += 1
+            print("Starting epoch %s" % epoch)
      
 
 if __name__ == '__main__':
